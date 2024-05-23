a4c1f1085ee620b85b59727823a909374ba9e473
tadi2017 <162723256+tadi2017@users.noreply.github.com>
2024-05-23 18:51:42 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,23 @@
 def enumerate_list(list):
-    return list
+    lista_1 = []
+    lista_2 = []
+    for element in list:
+        if element != "":
+            lista_1.append(element)
+            print(lista_1)
+    for index in range(len(lista_1)):
+            lista_2.append(f"{index}. {lista_1[index]}")
+    return lista_2
 
 
 def enumerate_backwards(list):
-    return list
+    lista_1 = []
+    lista_2 = []
+    for element in list:
+        if element != "":
+             lista_1.append(element[::-1])
+             print(lista_1)
+    for index in range(len(lista_1)):
+         lista_2.append(f"{index}. {lista_1[index]}")
+    return lista_2
+    
